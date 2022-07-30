c0f6fd0f0ad2b16625c6086fd35c7bcc5d837864
Hendrik Mans <hendrik@mans.de>
2022-07-30 14:42:17 +0200

    Clamp deltatimes

--- packages/timeline-composer/src/util/useAnimationFrame.ts
+++ packages/timeline-composer/src/util/useAnimationFrame.ts
@@ -1,17 +1,24 @@
 import { useLayoutEffect } from "react"
+import { clamp01 } from "./clamp"
 
 export const useAnimationFrame = (callback: FrameRequestCallback, deps?: any[]) => {
 	useLayoutEffect(() => {
 		let running = true
-		let lastTime = 0
+		let lastTime: number
 
 		const tick = (time: number) => {
+			/* Initialize lastTime if it doesn't exist. */
+			if (!lastTime) lastTime = time
+
+			/* Determine delta time, clamped to 1 second max. */
 			const diff = time - lastTime
-			const dt = diff / 1000
+			const dt = clamp01(diff / 1000)
 			lastTime = time
 
+			/* Invoke callback. */
 			callback(dt)
 
+			/* Repeat! */
 			if (running) requestAnimationFrame(tick)
 		}
 
