fec9e15c752ef59282efc6acb547d3b01c34d419
Hendrik Mans <hendrik@mans.de>
2022-07-30 14:42:27 +0200

    Implement example

--- apps/examples/src/App.tsx
+++ apps/examples/src/App.tsx
@@ -1,17 +1,35 @@
-import { Delay, Repeat } from "timeline-composer"
+import { ReactNode } from "react"
+import { Delay, Lifetime, Repeat } from "timeline-composer"
+
+const Box = ({ children }: { children?: ReactNode }) => (
+	<div style={{ border: "1px solid black", padding: "1em", marginTop: "1em" }}>
+		{children}
+	</div>
+)
 
 function App() {
 	return (
 		<>
 			<h1>Timeline Composer Test</h1>
-
 			<p>I'm immediately visible.</p>
-
-			<Repeat interval={2} times={3}>
-				<p>A second from now, something will appear here:</p>
-
+			<p>
+				A second from now, you will see a simple "animation", ahem, and it will repeat
+				every 6 seconds:
+			</p>
+			<Repeat interval={6}>
 				<Delay seconds={1}>
-					<p>I'm visible after a second.</p>
+					<Box>
+						I'm visible after a second. Something else will appear{" "}
+						<strong>after 2 seconds</strong>:
+						<Delay seconds={2}>
+							<Lifetime seconds={2}>
+								<Box>
+									I'm visible after two seconds, but will{" "}
+									<strong>disappear again after 2 seconds</strong>. See you later!
+								</Box>
+							</Lifetime>
+						</Delay>
+					</Box>
 				</Delay>
 			</Repeat>
 		</>
